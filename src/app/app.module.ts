2571c7688e638a8124a26f8ec0b424047f234670
anitesh0203 <aniteshkumar2012@gmail.com>
2017-12-12 19:06:53 -0500

    pushing the changes for service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UsertableComponent } from './components/usertable/usertable.component';
 import {MatTableModule} from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
 import {UserService} from './services/user.service';
+import {DataSyncService} from './services/data-sync.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +19,7 @@ import {UserService} from './services/user.service';
     MatTableModule,
     HttpClientModule,
   ],
-  providers: [UserService],
+  providers: [UserService,DataSyncService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
